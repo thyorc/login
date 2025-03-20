0bd47ce891e7007f4d265938b83f75849d35b6ba
Thiago <moura.francisco.thiago@hotmail.com>
2025-03-19 20:28:42 -0400

    Verify login by command line with hashed password

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {
+	username string
+	pass     string
+}
+
+func hashPass(pass string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func comparePass(hashedPass, pass string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPass), []byte(pass))
+	return err != nil
+}
+
+func compareLogin(validate, login user) error {
+	if validate.username != login.username {
+		return fmt.Errorf("invalid username")
+	}
+	if comparePass(validate.pass, login.pass) {
+		return fmt.Errorf("invalid password")
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world!")
+	pass, err := hashPass("admin")
+	if err != nil {
+		panic(err)
+	}
+
+	validate := user{username: "admin", pass: pass}
+	login := user{}
+
+	fmt.Print("Enter to username: ")
+	fmt.Scanf("%s", &login.username)
+
+	fmt.Print("Enter to pass: ")
+	fmt.Scanf("%s", &login.pass)
+
+	err = compareLogin(validate, login)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("Success access!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/thyorc/login
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.23.7
+
+require golang.org/x/crypto v0.36.0
